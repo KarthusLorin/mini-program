2ab025e0d0e9d18bfac69b0b8254695e8677b7c4
KarthusLorin <karthuslorin@gmail.com>
2018-09-13 23:36:41 +0800

    add like

---
+++ components/like/index.js
@@ -0,0 +1,25 @@
+// components/like/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onLike (event) {
+      console.log(event)
+    }
+  }
+})
