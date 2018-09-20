9cbff475690c4674f6e4f2445a91746504917821
KarthusLorin <karthuslorin@gmail.com>
2018-09-20 23:36:32 +0800

    complete like

--- components/like/index.js
+++ components/like/index.js
@@ -4,14 +4,20 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    like: {
+      type: Boolean
+    },
+    count: {
+      type: Number
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    yesSrc: 'images/like.png',
+    noSrc: 'images/like@dis.png'
   },
 
   /**
@@ -19,7 +25,14 @@ Component({
    */
   methods: {
     onLike (event) {
-      console.log(event)
+      let like = this.properties.like
+      let count = this.properties.count
+
+      count = like ? count - 1 : count + 1
+      this.setData({
+        count,
+        like: !like
+      })
     }
   }
 })
