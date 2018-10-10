91ca7b1f1f809de469809dbf4c16eb2987820d9e
KarthusLorin <karthuslorin@gmail.com>
2018-10-10 23:29:49 +0800

    add music

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,5 +1,7 @@
 import {classicBeh} from '../classic-beh.js'
 
+const mMgr = wx.getBackgroundAudioManager()
+
 Component({
   behaviors: [classicBeh],
   /**
@@ -13,6 +15,7 @@ Component({
    * 组件的初始数据
    */
   data: {
+    playing: false,
     pauseSrc: 'images/player@pause.png',
     playSrc: 'images/player@play.png'
   },
@@ -21,6 +24,19 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onPlay () {
+      if (!this.data.playing) {
+        // 切换图片
+        this.setData({
+          playing: true
+        })
+        mMgr.src = this.properties.src
+      } else {
+        this.setData({
+          playing: false
+        })
+        mMgr.pause()
+      }
+    }
   }
 })
