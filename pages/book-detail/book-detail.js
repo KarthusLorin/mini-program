132816c59ed5ed80e1166227170ffdd5d33fc6e4
KarthusLorin <karthuslorin@gmail.com>
2018-10-23 21:38:42 +0800

    init comment

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -2,7 +2,12 @@ import {
   BookModel
 } from '../../models/book.js'
 
+import {
+  LikeModel
+} from '../../models/like.js'
+
 const bookModel = new BookModel()
+const likeModel = new LikeModel()
 
 Page({
 
@@ -13,7 +18,8 @@ Page({
     comments: [],
     book: null,
     likeStatus: false,
-    likeCount: 0
+    likeCount: 0,
+    posting: false
   },
 
   /**
@@ -42,6 +48,23 @@ Page({
         likeStatus: res.like_status,
         likeCount: res.fav_nums
       })
+    })
+  },
+
+  onLike(event) {
+    const like_or_cancel = event.detail.behavior
+    likeModel.like(like_or_cancel, this.data.book.id, 400)
+  },
+
+  onFakePost (event) {
+    this.setData({
+      posting: true
+    })
+  },
+
+  onCancel (event) {
+    this.setData({
+      posting: false
     })
   },
 
